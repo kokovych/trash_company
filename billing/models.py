8428d83cf731a89f3da55691dc7f39f20b82291d
kokovych <kokovych@gmail.com>
2018-10-13 22:15:39 +0300

    add last_update to Bill model

--- billing/models.py
+++ billing/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 from account.models import PersonalAccount
 
 
 class Bill(models.Model):
     user_account = models.OneToOneField(PersonalAccount, on_delete=models.CASCADE)
     bill = models.FloatField(blank=True, default=0.0)
+    last_update = models.DateTimeField( default=timezone.now)
+
